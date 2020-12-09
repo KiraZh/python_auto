7f9a79f7e76a5967509c0398d2f634f7ee41ef49
Harrold Zhou <harroldzhou@gmail.com>
2020-12-09 10:03:30 +0800

    request post completed

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import requests
 
 if __name__ == "__main__":
-    header = {'Cookie': 'JSESSIONID=a098a56d-29da-48e1-b773-b3a3d0ceda6d'}
-    r = requests.get("https://xgaffairs.uestc.edu.cn/wxvacation/./checkRegisterNew", headers=header)
-    print(r.text)
+    url = "https://xgaffairs.uestc.edu.cn/wxvacation/./monitorRegisterForReturned"
 
+    payload = "{\n    \"healthCondition\":\"正常\",\n    \"todayMorningTemperature\":\"36°C~36.5°C\"," \
+              "\n    \"yesterdayEveningTemperature\":\"36°C~36.5°C\"," \
+              "\n    \"yesterdayMiddayTemperature\":\"36°C~36.5°C\"\n} "
+    headers = {
+        'Cookie': 'JSESSIONID=140ad9c7-db8c-4ea5-81ad-e24f1e9f3c80; SESSION=8d990cb9-9e47-4565-bda0-7db239f3284c',
+        'Content-Type': 'application/json;charset=UTF-8'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload.encode("UTF-8"))
+    print(response.text)
