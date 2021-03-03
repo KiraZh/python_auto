b3848a78ba4fe81ec90840821e335b492ebcb351
Harrold Zhou <harroldzhou@gmail.com>
2021-03-03 12:13:53 +0800

    add school auto check feature

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import requests
 
 if __name__ == "__main__":
-    url = "https://xgaffairs.uestc.edu.cn/wxvacation/./monitorRegisterForReturned"
+    url = "https://jzsz.uestc.edu.cn/wxvacation/./monitorRegisterForReturned"
 
-    payload = "{\n    \"healthCondition\":\"正常\",\n    \"todayMorningTemperature\":\"36°C~36.5°C\"," \
-              "\n    \"yesterdayEveningTemperature\":\"36°C~36.5°C\"," \
-              "\n    \"yesterdayMiddayTemperature\":\"36°C~36.5°C\"\n} "
+    payload = "{\n  \"healthCondition\" : \"正常\",\n  \"todayMorningTemperature\" : \"36°C~36.5°C\",\n  \"yesterdayEveningTemperature\" : \"36°C~36.5°C\",\n  \"yesterdayMiddayTemperature\" : \"36°C~36.5°C\",\n  \"location\" : \"四川省成都市郫都区成灌高速公路\"\n}"
+
     headers = {
-        'Cookie': 'JSESSIONID=140ad9c7-db8c-4ea5-81ad-e24f1e9f3c80; SESSION=8d990cb9-9e47-4565-bda0-7db239f3284c',
-        'Content-Type': 'application/json;charset=UTF-8'
+        'Cookie': 'JSESSIONID=89e4d4c5-7bbd-419a-bccb-100f017842b9; SESSION=e1c46237-5082-4276-a2b1-179ed3b6608a',
+        'Content-Type': 'application/json'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload.encode("UTF-8"))
     print(response.text)
-
-
-
-
